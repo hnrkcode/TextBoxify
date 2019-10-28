8abc22805373e70c89016905dae2a8a4e4e2ca5f
Henrik Petersson <henrik@tutamail.com>
2019-10-28 20:51:29 +0100

    Customize how imports work with __init__.py

---
+++ textboxify/__init__.py
@@ -0,0 +1,8 @@
+"""TextBoxify is a packages for creating dialog boxes in games.
+
+The purpose of this package is to easily implement dialog boxes
+in games created with Pygame. The boxes can be simple text or
+more elaborated boxes with animations and borders, because the
+package offers the ability to easily customize the boxes."""
+
+from .textbox import TextBox, TextBoxFrame
