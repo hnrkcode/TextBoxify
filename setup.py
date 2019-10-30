e4c207d6d4e7566152607e6962c10aa5f9466777
Henrik Petersson <henrik@tutamail.com>
2019-10-30 10:57:41 +0100

    Update metadata in setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,17 @@ import setuptools
 
 setuptools.setup(
     name="TextBoxify",
-    description="Text box engine for games written in Pygame.",
+    version="0.1.0",
+    author="Henrik Petersson",
+    author_email="henrik@tutamail.com",
+    url="https://github.com/hnrkcode/TextBoxify",
+    description="Pygame package to easily create dialog boxes for games.",
     packages=setuptools.find_packages(),
     package_data={
         "textboxify": [
             "data/border/default/*.png",
             "data/indicator/*.png",
             "data/portrait/*.png",
-        ]
+        ],
     },
 )
